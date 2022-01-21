b3319dd6f80ba086bb12147183de392e1e0aa3d0
Herman <hermankulik1995@gmail.com>
2022-01-21 08:13:35 +0100

    change 'times()' method to avoid side effect and implement immutability

--- src/test/java/com/training/tdd/CurrencyCalculatorTest.java
+++ src/test/java/com/training/tdd/CurrencyCalculatorTest.java
@@ -13,8 +13,11 @@ public class CurrencyCalculatorTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+
+        Dollar result = five.times(2);
+        assertEquals(10, result.amount);
+        result = five.times(3);
+        assertEquals(15, result.amount);
     }
 
 }
