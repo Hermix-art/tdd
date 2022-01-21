6755afd10f6b287250f8e1c33cf989a1fee8a5b7
Herman <hermankulik1995@gmail.com>
2022-01-21 07:38:36 +0100

    add first unworking test with stubs to compile it

---
+++ src/main/java/com/training/tdd/Dollar.java
@@ -0,0 +1,13 @@
+package com.training.tdd;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+
+    }
+
+    public void times(int multiplier) {
+
+    }
+}
--- src/test/java/com/training/tdd/CurrencyCalculatorTest.java
+++ src/test/java/com/training/tdd/CurrencyCalculatorTest.java
@@ -1,9 +1,20 @@
 package com.training.tdd;
 
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CurrencyCalculatorTest {
 
-
+    //calculation $5*2=$10
+    //Make amount private
+    //Dollar side-effects
+    //Money rounding
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
 
 }
