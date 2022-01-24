4118f0f88a5388691f058b98087d2b05ed2af7a3
Herman <hermankulik1995@gmail.com>
2022-01-24 22:15:17 +0100

    add Bank object as argument to 'reduce()'

--- src/main/java/com/training/tdd/Bank.java
+++ src/main/java/com/training/tdd/Bank.java
@@ -2,9 +2,10 @@ package com.training.tdd;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String chf, String usd, int i) {
+
     }
 }
