f899032c3dbea7073fbf30c80f5188d5cc534d1c
Herman <hermankulik1995@gmail.com>
2022-01-24 22:21:50 +0100

    add Pair class to reflect differenct ccy rates

---
+++ src/main/java/com/training/tdd/Pair.java
@@ -0,0 +1,14 @@
+package com.training.tdd;
+
+/**
+ * Reflects the pair of currency rates
+ */
+public class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+}
